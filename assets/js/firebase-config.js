86d0db86aac6c60ae01de91c529b4e21d552c698
VisionUP <79351666+VUcompany@users.noreply.github.com>
2024-12-26 18:52:31 +0900

    Initial commit

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBdCs8WbciifFZ9tLBjC8l7bYIBsKZMJjk",
+  authDomain: "velder-381f3.firebaseapp.com",
+  projectId: "velder-381f3",
+  storageBucket: "velder-381f3.appspot.com",
+  messagingSenderId: "1024405768460",
+  appId: "1:1024405768460:web:f129e18ddc2f354895a129",
+  measurementId: "G-40QV2X2M8Z",
+};
+
+export default firebaseConfig;
